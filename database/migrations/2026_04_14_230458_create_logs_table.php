6d6ee69a7d0132dce23a6aaa1b217490a40e3d06
Angel David Agudelo <angeldavidagudelocuartas13@gmail.com>
2026-04-14 18:08:17 -0500

    chore(database): Implementación de la base de datos inicial para el proyecto AutoFlow. Posiblemente no versión final, pero si ideal para arrancar

---
+++ database/migrations/2026_04_14_230458_create_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('automation_id')->constrained('automations')->onDelete('cascade');
+            $table->string('trigger_type', 100);
+            $table->enum('status', ['success', 'failed']);
+            $table->text('result')->nullable();
+            $table->timestamp('executed_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logs');
+    }
+};
---
+++ database/migrations/2026_04_14_230543_create_executions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('executions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('automation_id')->constrained('automations')->onDelete('cascade');
+            $table->enum('status', ['pending', 'processing', 'completed', 'failed']);
+            $table->integer('attempt')->default(0);
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('finished_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('executions');
+    }
+};
